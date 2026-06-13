2c985d77e62f798a0ddefbb6d294db3db1ed1ec6
Alon Hearter <barlevalon@users.noreply.github.com>
2026-06-13 22:17:16 +0300

    app: move session wiring out of root widget
    Keep MklatApp focused on provider and MaterialApp composition while AppSession owns service construction, polling callbacks, location sync, and lifecycle policy.
    
    Validation:
    - flutter test
    - flutter analyze
    - git diff --check

---
+++ lib/application/app_session.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+import '../data/services/http_client.dart';
+import '../data/services/oref_alerts_service.dart';
+import '../data/services/oref_districts_service.dart';
+import '../data/services/oref_history_service.dart';
+import '../data/services/polling_manager.dart';
+import '../data/services/rss_news_service.dart';
+import '../presentation/providers/alerts_provider.dart';
+import '../presentation/providers/connectivity_provider.dart';
+import '../presentation/providers/location_provider.dart';
+import '../presentation/providers/news_provider.dart';
+
+class AppSession {
+  final bool pollingEnabled;
+  final bool bootstrapEnabled;
+
+  late final HttpClient httpClient;
+  late final PollingManager pollingManager;
+  late final LocationProvider locationProvider;
+  late final AlertsProvider alertsProvider;
+  late final NewsProvider newsProvider;
+  late final ConnectivityProvider connectivityProvider;
+
+  late final OrefDistrictsService _districtsService;
+
+  AppSession({
+    http.Client? client,
+    ConnectivityProvider? connectivityProvider,
+    this.pollingEnabled = true,
+    this.bootstrapEnabled = true,
+  }) {
+    httpClient = HttpClient(client: client);
+    final alertsService = OrefAlertsService(httpClient);
+    final historyService = OrefHistoryService(httpClient);
+    final newsService = RssNewsService(httpClient);
+    _districtsService = OrefDistrictsService(httpClient);
+
+    pollingManager = PollingManager(
+      alertsService: alertsService,
+      historyService: historyService,
+      newsService: newsService,
+    );
+
+    locationProvider = LocationProvider();
+    alertsProvider = AlertsProvider();
+    newsProvider = NewsProvider();
+    this.connectivityProvider = connectivityProvider ?? ConnectivityProvider();
+
+    _wirePollingCallbacks();
+    locationProvider.addListener(_onLocationChange);
+  }
+
+  void start() {
+    if (!bootstrapEnabled) return;
+
+    locationProvider.loadLocations();
+    locationProvider.loadAvailableLocations(_districtsService);
+    connectivityProvider.initialize();
+    if (pollingEnabled) {
+      pollingManager.start();
+    }
+  }
+
+  void handleLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        if (bootstrapEnabled && pollingEnabled) {
+          alertsProvider.setResuming(true);
+          pollingManager.start();
+        }
+        break;
+      case AppLifecycleState.paused:
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.detached:
+      case AppLifecycleState.hidden:
+        pollingManager.stop();
+        break;
+    }
+  }
+
+  void dispose() {
+    locationProvider.removeListener(_onLocationChange);
+    pollingManager.dispose();
+    httpClient.dispose();
+  }
+
+  void _wirePollingCallbacks() {
+    pollingManager.onAlertData = (currentAlerts, alertHistory) {
+      connectivityProvider.reportHttpSuccess();
+      alertsProvider.onAlertData(currentAlerts, alertHistory);
+    };
+    pollingManager.onNewsData = (newsItems) {
+      connectivityProvider.reportHttpSuccess();
+      newsProvider.onNewsData(newsItems);
+    };
+    pollingManager.onAlertError = (error) {
+      connectivityProvider.reportHttpFailure();
+      alertsProvider.onError(error);
+    };
+    pollingManager.onAlertHistoryError = (error) {
+      alertsProvider.onHistoryError(error);
+    };
+    pollingManager.onNewsError = (error) {
+      connectivityProvider.reportHttpFailure();
+      newsProvider.onError(error);
+    };
+  }
+
+  void _onLocationChange() {
+    final primary = locationProvider.primaryLocation;
+    alertsProvider.setPrimaryLocation(primary?.orefName);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
+import 'application/app_session.dart';
 import 'core/app_theme.dart';
-import 'data/services/http_client.dart';
-import 'data/services/oref_alerts_service.dart';
-import 'data/services/oref_districts_service.dart';
-import 'data/services/oref_history_service.dart';
-import 'data/services/rss_news_service.dart';
-import 'data/services/polling_manager.dart';
-import 'presentation/providers/location_provider.dart';
-import 'presentation/providers/alerts_provider.dart';
-import 'presentation/providers/news_provider.dart';
 import 'presentation/providers/connectivity_provider.dart';
 import 'presentation/app_shell.dart';
 
@@ -38,103 +30,29 @@ class MklatApp extends StatefulWidget {
 }
 
 class _MklatAppState extends State<MklatApp> with WidgetsBindingObserver {
-  late final HttpClient _httpClient;
-  late final PollingManager _pollingManager;
-  late final LocationProvider _locationProvider;
-  late final AlertsProvider _alertsProvider;
-  late final NewsProvider _newsProvider;
-  late final ConnectivityProvider _connectivityProvider;
+  late final AppSession _session;
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
-    _initializeServices();
-  }
-
-  void _initializeServices() {
-    _httpClient = HttpClient(client: widget.httpClient);
-    final alertsService = OrefAlertsService(_httpClient);
-    final historyService = OrefHistoryService(_httpClient);
-    final newsService = RssNewsService(_httpClient);
-    final districtsService = OrefDistrictsService(_httpClient);
-
-    _pollingManager = PollingManager(
-      alertsService: alertsService,
-      historyService: historyService,
-      newsService: newsService,
-    );
-
-    _locationProvider = LocationProvider();
-    _alertsProvider = AlertsProvider();
-    _newsProvider = NewsProvider();
-    _connectivityProvider =
-        widget.connectivityProvider ?? ConnectivityProvider();
-
-    // Wire polling callbacks to providers
-    _pollingManager.onAlertData = (currentAlerts, alertHistory) {
-      _connectivityProvider.reportHttpSuccess();
-      _alertsProvider.onAlertData(currentAlerts, alertHistory);
-    };
-    _pollingManager.onNewsData = (newsItems) {
-      _connectivityProvider.reportHttpSuccess();
-      _newsProvider.onNewsData(newsItems);
-    };
-    _pollingManager.onAlertError = (error) {
-      _connectivityProvider.reportHttpFailure();
-      _alertsProvider.onError(error);
-    };
-    _pollingManager.onAlertHistoryError = (error) {
-      _alertsProvider.onHistoryError(error);
-    };
-    _pollingManager.onNewsError = (error) {
-      _connectivityProvider.reportHttpFailure();
-      _newsProvider.onError(error);
-    };
-
-    // Listen for location changes to update state machine
-    _locationProvider.addListener(_onLocationChange);
-
-    // Initialize
-    if (widget.bootstrapEnabled) {
-      _locationProvider.loadLocations();
-      _locationProvider.loadAvailableLocations(districtsService);
-      _connectivityProvider.initialize();
-      if (widget.pollingEnabled) {
-        _pollingManager.start();
-      }
-    }
-  }
-
-  void _onLocationChange() {
-    final primary = _locationProvider.primaryLocation;
-    _alertsProvider.setPrimaryLocation(primary?.orefName);
+    _session = AppSession(
+      client: widget.httpClient,
+      connectivityProvider: widget.connectivityProvider,
+      pollingEnabled: widget.pollingEnabled,
+      bootstrapEnabled: widget.bootstrapEnabled,
+    )..start();
   }
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-    switch (state) {
-      case AppLifecycleState.resumed:
-        if (widget.bootstrapEnabled && widget.pollingEnabled) {
-          _alertsProvider.setResuming(true);
-          _pollingManager.start();
-        }
-        break;
-      case AppLifecycleState.paused:
-      case AppLifecycleState.inactive:
-      case AppLifecycleState.detached:
-      case AppLifecycleState.hidden:
-        _pollingManager.stop();
-        break;
-    }
+    _session.handleLifecycleState(state);
   }
 
   @override
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
-    _locationProvider.removeListener(_onLocationChange);
-    _pollingManager.dispose();
-    _httpClient.dispose();
+    _session.dispose();
     super.dispose();
   }
 
@@ -142,10 +60,10 @@ class _MklatAppState extends State<MklatApp> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider.value(value: _locationProvider),
-        ChangeNotifierProvider.value(value: _alertsProvider),
-        ChangeNotifierProvider.value(value: _newsProvider),
-        ChangeNotifierProvider.value(value: _connectivityProvider),
+        ChangeNotifierProvider.value(value: _session.locationProvider),
+        ChangeNotifierProvider.value(value: _session.alertsProvider),
+        ChangeNotifierProvider.value(value: _session.newsProvider),
+        ChangeNotifierProvider.value(value: _session.connectivityProvider),
       ],
       child: MaterialApp(
         title: 'mklat.news',
